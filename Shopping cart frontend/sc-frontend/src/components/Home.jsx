00a1eb833f902d128ddf3177b7127708beb573c3
rawatananya44 <ananya_rawat@optum.com>
2024-06-07 13:22:57 +0530

    .

--- Shopping cart frontend/sc-frontend/src/components/Home.jsx
+++ Shopping cart frontend/sc-frontend/src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div >
       <img src={homeImage} style={{height: '100%', width:'100%', backgroundRepeat:'repeat-y', backgroundSize:'cover'}} alt=""/>
       </div>   
-      <h1>hi</h1>     
+         
     </div>
   )
 }
